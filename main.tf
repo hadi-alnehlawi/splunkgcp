871911db208d44fc89dcfc5af75b43816883627a
Hadi Alnehlawi <nhadi82@hotmail.com>
2022-06-23 12:27:44 -0300

    logsin destination

--- main.tf
+++ main.tf
@@ -3,8 +3,7 @@ resource "google_logging_organization_sink" "org-sink" {
   name   = "org-sink"
   description = "demo sink on org level"
   org_id = var.org_id
-  # destination = "pubsub.googleapis.com/projects/${var.project_id}/topics/${google_pubsub_topic.org-sink-pubsub.topic_id}"
-  destination = google_pubsub_topic.org-sink-pubsub.id
+  destination = "pubsub.googleapis.com/${google_pubsub_topic.org-sink-pubsub.id}"
   # Log all WARN or higher severity messages relating to instances
   filter = "resource.type = gce_instance AND severity >= WARNING"
 }
